2a2c44f559a13a61897e0eb43b40b02a5d63427b
ladyyasita <121614535+ladyyasita@users.noreply.github.com>
2025-03-25 00:38:31 +0100

    created react

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header classNameApp-header> <img src={logo} className="App-logo alt-fluid" alt="alt-fluid" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
